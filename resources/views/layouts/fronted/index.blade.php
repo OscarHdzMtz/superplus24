41e02516b275b9bb2fa0580804be22b3ea20968a
OscarHernandez <oskr9922@gmail.com>
2021-08-17 13:34:12 -0500

    se hicieron mas modificaciones para hacer local los archivos de CSS y Js de frontEnd excepto Iconos

--- resources/views/layouts/fronted/index.blade.php
+++ resources/views/layouts/fronted/index.blade.php
@@ -1,80 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" type="imagenes" href="{{asset('img/favicon.ico')}}">
+    <link rel="shortcut icon" type="imagenes" href="{{ asset('img/favicon.ico') }}">
     <title>SuperPlus</title>
 
-    <!--LINNK-->
+    <!--ESTILO ICONOS Y TIPOS DE LETRAS-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&display=swap" rel="stylesheet">
+    <link type="text/css" href="{{ asset('css/styleletras.css') }}" rel="stylesheet">
+
+    {{-- ESTILO BOOTSTRAP --}}
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> --}}
+    {{-- local --}}
+    <link type="text/css" href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
 
     {{-- animaciones al hacer scroll --}}
     {{-- <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" /> --}}
-    <link type="text/css" href="{{asset('css/aos.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('css/aos.css') }}" rel="stylesheet">
 
     {{-- estilo de prueba --}}
-    <link type="text/css" href="{{asset('css/stylenos.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('css/stylenos.css') }}" rel="stylesheet">
 
-    <!-- Styles -->
-    <link type="text/css" href="{{asset('css/style.css') }}" rel="stylesheet">
-    <link type="text/css" href="{{asset('css/slick.css') }}" rel="stylesheet">
+    <!-- ESTILOS PROPIOD -->
+    <link type="text/css" href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ asset('css/slick.css') }}" rel="stylesheet">
 </head>
-<body> 
+
+<body>
     {{-- @yield('redes') --}}
-        {{-- <section id="headerSection"> --}}
-            {{-- @yield('navbar_top') --}}
-            @yield('navbar')
-            @yield('banner')
-        {{-- </section> --}}
+    {{-- <section id="headerSection"> --}}
+    {{-- @yield('navbar_top') --}}
+    @yield('navbar')
+    @yield('banner')
+    {{-- </section> --}}
     @yield('cards_service')
     @yield('title')
-        <div class="body_cards">
-            @yield('cards')
-        </div>
+    <div class="body_cards">
+        @yield('cards')
+    </div>
     @yield('title5')
-        @yield('Proveedores')
+    @yield('Proveedores')
     @yield('title2')
     @yield('products')
     @yield('footer')
     @yield('modals')
     @yield('whats')
+
+
     <!--SCRIPT-->
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="{{asset('js/hgnka.js') }}"></script>
-    <script src="{{asset('js/security.js') }}"></script>
-    <script src="{{asset('js/provee.js') }}"></script>
-    <script src="{{asset('js/slick.min.js') }}"></script>
-    <script src="{{asset('js/typed.js') }}"></script>
-{{-- js de prueba --}}
-    <script src="{{asset('js/prueba.js') }}"></script>
+    {{-- ONLINE --}}
+    {{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+                        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> --}}
+    {{-- LOCAL --}}
+    <script src="{{ asset('js/jquery-3.5.1.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+    <script src="{{ asset('js/hgnka.js') }}"></script>
+    <script src="{{ asset('js/security.js') }}"></script>
+    <script src="{{ asset('js/provee.js') }}"></script>
+    <script src="{{ asset('js/slick.min.js') }}"></script>
+    <script src="{{ asset('js/typed.js') }}"></script>
+    {{-- js de prueba --}}
+    <script src="{{ asset('js/prueba.js') }}"></script>
 
     {{-- aimaciones al hacer scroll --}}
     {{-- <script src="https://unpkg.com/aos@next/dist/aos.js"></script> --}}
-    <script src="{{asset('js/aos.js') }}"></script>
-
+    <script src="{{ asset('js/aos.js') }}"></script>
     <script>
         AOS.init({
             easing: 'ease-in-out-sine',
             duration: 1000
-        });            
-   </script>
+        });
+    </script>
 
-
-{{--  --}}
+    {{-- texto en movimiento --}}
     <script>
-        var typed = new Typed('.type', {            
-        strings: ['<span><i class="fas fa-check"></i></span> DALE UN PLUS A TU DIA!!', 
-                    '<span><i class="fas fa-building"></i></span> SERIVICIO LAS 24 HORAS'                    
-                    ,'<span><i class="fas fa-motorcycle"></i></span> SERVICIO A DOMICILIO'],
-        typeSpeed:60,backSpeed:60,loop:true});
+        var typed = new Typed('.type', {
+            strings: ['<span><i class="fas fa-check"></i></span> DALE UN PLUS A TU DIA!!',
+                '<span><i class="fas fa-building"></i></span> SERIVICIO LAS 24 HORAS',
+                '<span><i class="fas fa-motorcycle"></i></span> SERVICIO A DOMICILIO'
+            ],
+            typeSpeed: 60,
+            backSpeed: 60,
+            loop: true
+        });
     </script>
 
 </body>
--- resources/views/layouts/fronted/promociones.blade.php
+++ resources/views/layouts/fronted/promociones.blade.php
@@ -1,78 +1,92 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" type="imagenes" href="{{asset('img/favicon.ico')}}">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-        <title>Promociones</title>
 
-        <!-- Fonts--> 
-        <link href="{{asset('css/stylenos.css') }}" rel="stylesheet">
-        <link href="{{asset('css/style.css') }}" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <!-- Styles -->
-    </head>
-    <body>
-        @yield('redes')
-        <section id="headerSection_promo">
-            @yield('navbar_top')
-            @yield('navbar')
-            @yield('banner')
-        </section>
-        {{-- <div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="imagenes" href="{{ asset('img/favicon.ico') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> --}}
+    {{-- local --}}
+    <link type="text/css" href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <title>Promociones</title>
+
+    <!-- Fonts-->
+    <link href="{{ asset('css/stylenos.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Styles -->
+</head>
+
+<body>
+    @yield('redes')
+    <section id="headerSection_promo">
+        @yield('navbar_top')
+        @yield('navbar')
+        @yield('banner')
+    </section>
+    {{-- <div>
             @yield('timespace')
         </div> --}}
-        @yield('title')
-       {{--  <div class="body_clientes">
+    @yield('title')
+    {{-- <div class="body_clientes">
             @yield('clientes')
         </div> --}}
-        {{-- @yield('title2') --}}
-        @yield('foda')    
-        @yield('footer') 
-    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-   {{-- se agragro bootstrap.min.js para que sirva navbar en modo cel --}}
-   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
-   
+    {{-- @yield('title2') --}}
+    @yield('foda')
+    @yield('footer')
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
+    {{-- se agragro bootstrap.min.js para que sirva navbar en modo cel --}}
+    {{-- LOCAL --}}
+    <script src="{{ asset('js/jquery-3.5.1.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
-    <script src="{{asset('js/responsive.js') }}"></script>
-    <script src="{{asset('js/security.js') }}"></script>
-    <script src="{{asset('js/nosotros.js') }}"></script>
+
+    <script src="{{ asset('js/responsive.js') }}"></script>
+    <script src="{{ asset('js/security.js') }}"></script>
+    <script src="{{ asset('js/nosotros.js') }}"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script>
-	var swiper = new Swiper('.swiper-container', {
-        effect: 'coverflow',grabCursor: true,centeredSlides: true,
-        slidesPerView: 'auto',
-        coverflowEffect: {
-        rotate: 20,
-        stretch: 0,
-        depth: 200,
-        modifier: 1,
-        slideShadows: true,
-        },
-        loop:true,
-	    autoplay:{
-			delay:3500,
-			disableOnInteraction:false,
-        },
-    });	
+        var swiper = new Swiper('.swiper-container', {
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+                rotate: 20,
+                stretch: 0,
+                depth: 200,
+                modifier: 1,
+                slideShadows: true,
+            },
+            loop: true,
+            autoplay: {
+                delay: 3500,
+                disableOnInteraction: false,
+            },
+        });
     </script>
-    <script src="{{asset('js/typed.js') }}"></script>
+    <script src="{{ asset('js/typed.js') }}"></script>
 
     {{-- estilo nuevo navbar --}}
-    <script src="{{asset('js/prueba.js') }}"></script>
+    <script src="{{ asset('js/prueba.js') }}"></script>
 
     <script>
-	var typed = new Typed('.type', {
-    strings: ['<span><i class="fas fa-check"></i></span> DALE UN PLUS A TU DIA!!', 
-                    '<span><i class="fas fa-building"></i></span> SERIVICIO LAS 24 HORAS'                    
-                    ,'<span><i class="fas fa-motorcycle"></i></span> SERVICIO A DOMICILIO'],
-    typeSpeed:60,backSpeed:60,loop:true});
+        var typed = new Typed('.type', {
+            strings: ['<span><i class="fas fa-check"></i></span> DALE UN PLUS A TU DIA!!',
+                '<span><i class="fas fa-building"></i></span> SERIVICIO LAS 24 HORAS',
+                '<span><i class="fas fa-motorcycle"></i></span> SERVICIO A DOMICILIO'
+            ],
+            typeSpeed: 60,
+            backSpeed: 60,
+            loop: true
+        });
     </script>
-    </body>
+</body>
+
 </html>
