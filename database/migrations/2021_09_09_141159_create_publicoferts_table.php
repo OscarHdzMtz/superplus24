e28b637bcc72879f30912f2ed7d594896a413cbd
Oscar Hernandez Martinez <oskr9922@gmail.com>
2021-09-09 16:36:36 -0500

    se crearon migraciones de Promociones, nuevo productos y instalaciones para que se genere a automaticamte la tabla en la base da tos

---
+++ database/migrations/2021_09_09_141159_create_publicoferts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+class CreatePublicofertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publicoferts', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->integer("user_id");
+            $table->string("titulo");
+            $table->string("texto")->nullable();
+            $table->string("image");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publicoferts');
+    }
+}
