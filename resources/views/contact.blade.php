08ec2268fb3f97ac0a96cbf7c5d073063c31c883
Oscar Hernandez Martinez <oskr9922@gmail.com>
2021-11-12 12:50:57 -0600

    cambio de iconos de redes sociales en Contactanos

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -165,14 +165,11 @@
                         <i class="fab fa-facebook-f fa-1x"></i>
                     </a>
                     <a href="" target="none">
+                        <i class="fab fa-whatsapp"></i>
+                    </a>
+                    <a href="" target="none">
                         <i class="fab fa-instagram"></i>
-                    </a>
-                    <a href="" target="none">
-                        <i class="fab fa-twitter-square"></i>
-                    </a>
-                    <a href="" target="none">
-                        <i class="fab fa-linkedin"></i>
-                    </a>
+                    </a>                   
                 </div>
             </div>
         </div>
