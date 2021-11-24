ff1b638f3771fa2c42f3042d62de5b4618ac5bec
Oscar Hernandez Martinez <oskr9922@gmail.com>
2021-11-24 09:32:10 -0600

    cambio de diseño del formulario crear datos mi empresa

--- resources/views/miempresa/creatempresa.blade.php
+++ resources/views/miempresa/creatempresa.blade.php
@@ -21,10 +21,43 @@
             Datos mi empresa</small>
         </h1>
 
-        <div class="row text-center">
-            <div class="col-md-offset col-md-6 justify-content-center">                
-                {!! Form::open(['route' => 'miempresa.store', 'files' => 'true']) !!}
-                <form>
+        <div class="row text-center justify-content-md-center">
+            <div class="col-md-offset col-md-6">                
+                {!! Form::open(['route' => 'miempresa.store', 'files' => 'true']) !!}                
+                <div class="empresa-form">
+                    <form>                        
+                        <hr style="margin: 0 auto" class="style1">
+                        <div class="input-container ">
+                            <input type="text" name="orden" class="empresa_input" placeholder="orden" required>
+                        </div>
+                        <div class="input-container">
+                            <input type="text" name="label" class="empresa_input" placeholder="Label" required>
+                        </div>    
+                        <div class="input-container">
+                            {{-- <input id="" type="email" class="form-control contact_input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> --}}
+                            <input type="text" name="titulo" class="empresa_input" placeholder="Titulo" required>
+                        </div>                                          
+                        <div class="input-container">
+                            <textarea name="description" class="empresa_input" id="" placeholder="Descripcion"
+                                onkeyup="countChars(this);" required></textarea>
+                            <p id="charNum" class="text-success text-center">0 caracteres</p>
+                        </div>
+                        <div class="form-group">
+                            <label for="message-text" class="col-form-label">IMAGEN:</label>
+                            {{ Form::file('image', ['required' => 'required']) }}
+                        </div>
+                        <div class="form-group">
+                            <label for="message-text" class="col-form-label">IMAGEN SOBRE IMAGEN:</label>
+                            {{ Form::file('imghover') }}
+                        </div>
+    
+                        <div class="form-group">
+                            {!! Form::submit('Guardar', ['class' => 'btn btn-outline-success']) !!}
+                            <a href="{{ route('miempresa.index') }}" class="btn btn-outline-danger">Cancelar</a>
+                        </div>
+                    </form>
+                </div>
+               {{--  <form>
                     <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Orden :</label>
                         <input type="text" name="orden" class="form-control" id="recipient-name" required>
@@ -55,7 +88,7 @@
                         {!! Form::submit('Guardar', ['class' => 'btn btn-outline-success']) !!}
                         <a href="{{ route('miempresa.index') }}" class="btn btn-outline-danger">Cancelar</a>
                     </div>
-                </form>
+                </form> --}}
                 {!! Form::close() !!}
             </div>
         </div>
