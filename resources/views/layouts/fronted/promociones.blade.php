7f5b7eadcb4168f9bc2c7ba547f5413576993aab
Oscar Hernandez Martinez <oskr9922@gmail.com>
2023-05-11 17:42:42 -0600

    Quitando script tailwind en Head de promociones ya que no dejaba ver el menuñ

--- resources/views/layouts/fronted/promociones.blade.php
+++ resources/views/layouts/fronted/promociones.blade.php
@@ -31,8 +31,7 @@
     <!-- Fonts-->
     <link href="{{ asset('css/stylenos.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">    
     <!-- Styles -->
     @livewireStyles
 </head>
