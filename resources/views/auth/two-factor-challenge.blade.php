c1b8db7a974e969ddb6b79edb3c3ffb13d3af7ed
oskrHM <oskr9922@gmail.com>
2024-05-28 14:07:51 -0600

    empezando con el dashboard de administracion

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -1,4 +1,5 @@
 <x-guest-layout>
+    
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
@@ -6,11 +7,11 @@
 
         <div x-data="{ recovery: false }">
             <div class="mb-4 text-sm text-gray-600" x-show="! recovery">
-                {{ __('Please confirm access to your account by entering the authentication code provided by your authenticator application.') }}
+                {{ __('Confirme el acceso a su cuenta ingresando el código de autenticación proporcionado por su aplicación de autenticación') }}
             </div>
 
             <div class="mb-4 text-sm text-gray-600" x-cloak x-show="recovery">
-                {{ __('Please confirm access to your account by entering one of your emergency recovery codes.') }}
+                {{ __('Confirme el acceso a su cuenta ingresando uno de sus códigos de recuperación de emergencia.') }}
             </div>
 
             <x-validation-errors class="mb-4" />
@@ -19,26 +20,26 @@
                 @csrf
 
                 <div class="mt-4" x-show="! recovery">
-                    <x-label for="code" value="{{ __('Code') }}" />
-                    <x-input id="code" class="block mt-1 w-full" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
+                    <x-label for="code" value="{{ __('Codigo') }}" />
+                    <x-input id="code" class="block w-full mt-1" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
                 </div>
 
                 <div class="mt-4" x-cloak x-show="recovery">
-                    <x-label for="recovery_code" value="{{ __('Recovery Code') }}" />
-                    <x-input id="recovery_code" class="block mt-1 w-full" type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
+                    <x-label for="recovery_code" value="{{ __('Código de recuperación') }}" />
+                    <x-input id="recovery_code" class="block w-full mt-1" type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
                 </div>
 
                 <div class="flex items-center justify-end mt-4">
-                    <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
+                    <button type="button" class="text-sm text-gray-600 underline cursor-pointer hover:text-gray-900"
                                     x-show="! recovery"
                                     x-on:click="
                                         recovery = true;
                                         $nextTick(() => { $refs.recovery_code.focus() })
                                     ">
-                        {{ __('Use a recovery code') }}
+                        {{ __('Utilice un código de recuperación') }}
                     </button>
 
-                    <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
+                    <button type="button" class="text-sm text-gray-600 underline cursor-pointer hover:text-gray-900"
                                     x-cloak
                                     x-show="recovery"
                                     x-on:click="
@@ -49,7 +50,7 @@
                     </button>
 
                     <x-button class="ms-4">
-                        {{ __('Log in') }}
+                        {{ __('Iniciar Sesion') }}
                     </x-button>
                 </div>
             </form>
--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,10 +1,15 @@
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
+        <div class="font-medium text-red-600">{{ __('Algo salió mal.') }}</div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+        <ul class="mt-3 text-sm text-red-600 list-disc list-inside">
+            @foreach ($errors->all() as $error)                                
+                {{-- <li>{{ $error }}</li> --}}
+                @if ($error == "The provided two factor authentication code was invalid.")
+                    <li>El código de autenticación de dos factores proporcionado no es válido.</li>
+                @else
+                    <li>{{ $error }}</li>
+                @endif 
             @endforeach
         </ul>
     </div>
