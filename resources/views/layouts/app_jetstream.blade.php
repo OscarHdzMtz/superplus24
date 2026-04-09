7105541d0ae7a09c138751821af03b56402bb054
OscarHM <oskr9922@gmail.com>
2026-04-09 09:03:04 -0600

    Cambiando titulo en login

--- resources/views/layouts/app_jetstream.blade.php
+++ resources/views/layouts/app_jetstream.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="imagenes" href="{{ asset('img/estaticos/logopalomita.png') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'SuperPlus24') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
